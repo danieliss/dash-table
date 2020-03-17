f04bf25875bbfbf18a84cc7205b5aead6bd03d40
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2020-03-17 18:55:56 -0400

    also exclude children of select|delete phantom cells

--- src/dash-table/derived/table/fragments.tsx
+++ src/dash-table/derived/table/fragments.tsx
@@ -23,7 +23,11 @@ function renderFragment(cells: any[][] | null, offset: number = 0) {
 const getHiddenCell = (cell: JSX.Element) => React.cloneElement(cell, {
     ...cell.props,
     className: cell.props.className ? `${cell.props.className} phantom-cell` : 'phantom-cell'
-}, cell.type === 'th' ? null : cell.props.children);
+}, (
+    cell.type === 'th' ||
+    (cell.type === 'td' && cell.props.className && cell.props.className.indexOf('dash-select-cell') !== -1) ||
+    (cell.type === 'td' && cell.props.className && cell.props.className.indexOf('dash-delete-cell') !== -1)
+) ? null : cell.props.children);
 
 const isEmpty = (cells: JSX.Element[][] | null) =>
     !cells ||
